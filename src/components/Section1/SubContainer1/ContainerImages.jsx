bbb746698b391866c817da56cc146f8cdcd866a1
Gerónimo <geronimomcordoba@gmail.com>
2024-03-21 22:08:22 -0300

    asdasd

--- src/components/Section1/SubContainer1/ContainerImages.jsx
+++ src/components/Section1/SubContainer1/ContainerImages.jsx
@@ -23,7 +23,7 @@ const ContainerImages = () => {
         />
       ) : (
         <div className="small-images-container">
-          <img src="/react12888.png" alt="" />
+          <img src="/react12121288.png" alt="" />
           <img src="/nodejs128.png" alt="" />
           <img src="/js128.png" alt="" />
         </div>
