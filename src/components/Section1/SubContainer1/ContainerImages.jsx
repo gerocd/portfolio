0fc62109e75df29def09bc1fbf27ff0d6842f5f7
Gerónimo <geronimomcordoba@gmail.com>
2024-03-21 22:00:57 -0300

    ultimo intento de la fotooo

--- src/components/Section1/SubContainer1/ContainerImages.jsx
+++ src/components/Section1/SubContainer1/ContainerImages.jsx
@@ -23,7 +23,7 @@ const ContainerImages = () => {
         />
       ) : (
         <div className="small-images-container">
-          <img src="/react128 (2).png" alt="" />
+          <img src="/react12888.png" alt="" />
           <img src="/nodejs128.png" alt="" />
           <img src="/js128.png" alt="" />
         </div>
